43e48496ef6da756ad41dfe22550988288dc888d
Yang.Cai <1264755308@qq.com>
2022-08-02 15:40:48 +0800

    add grpc

--- pkg/cmd/gateway/main.go
+++ pkg/cmd/gateway/main.go
@@ -11,32 +11,32 @@ import (
 
 var g errgroup.Group
 
-func init() {
-	// db.New(&setting.DBSetting{
-	// 	Host:     "127.0.0.1",
-	// 	Port:     "3306",
-	// 	Username: "root",
-	// 	Password: "123456",
-	// 	DbName:   "comma",
-	// })
-}
+// func init() {
+// 	// db.New(&setting.DBSetting{
+// 	// 	Host:     "127.0.0.1",
+// 	// 	Port:     "3306",
+// 	// 	Username: "root",
+// 	// 	Password: "123456",
+// 	// 	DbName:   "comma",
+// 	// })
+// }
 
-func httpServerStart(serverAddr string) error {
-	s := httpServer.NewServer()
-	s.SetServerAddr(serverAddr)
-	// s.UseTrace(TRACE_URL, "gateway", serverAddr)
-	gatewayHttp.NewServer(s)
-	s.Start()
-	return nil
-}
+// func httpServerStart(serverAddr string) error {
+// 	s := httpServer.NewServer()
+// 	s.SetServerAddr(serverAddr)
+// 	// s.UseTrace(TRACE_URL, "gateway", serverAddr)
+// 	gatewayHttp.NewServer(s)
+// 	s.Start()
+// 	return nil
+// }
 
-func jsonRpcServerStart(serverAddr string) error {
-	s := jsonrpcServer.NewServer()
-	s.SetServerAddr(serverAddr)
-	s.RegisterService(gatewayJsonRpc.GetInstance())
-	s.Start()
-	return nil
-}
+// func jsonRpcServerStart(serverAddr string) error {
+// 	s := jsonrpcServer.NewServer()
+// 	s.SetServerAddr(serverAddr)
+// 	s.RegisterService(gatewayJsonRpc.GetInstance())
+// 	s.Start()
+// 	return nil
+// }
 
 func grpcServerStart(serverAddr string) error {
 	s := gatewayGrpc.NewServer()
